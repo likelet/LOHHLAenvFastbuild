64a4c38d3f3578fdbba58b7980844e1694d59c33
Qi ZHAO <zhaoqi@sysucc.org.cn>
2022-05-20 12:58:03 +0800

    Create Buildenv.sh

---
+++ Buildenv.sh
@@ -0,0 +1,29 @@
+#!/bin/sh 
+
+# asume you have anaconda installed in your system 
+
+# 1. first build base env from conda lohhla env 
+
+conda create -c bioconda -n lohhla lohhla 
+conda activate lohhla 
+
+# 2. reinstall proper version of depdencies 
+
+conda install -c bioconda jellyfish=2.2.6
+
+conda install -c bioconda samtools=1.3.1
+
+conda install -c bioconda bedtools=2.26.0
+
+conda install -c compbiocore novoalign
+
+
+# 3. install picard 
+wget https://github.com/broadinstitute/picard/releases/download/1.123/picard-tools-1.123.zip && \
+	unzip picard-tools-1.123.zip && \
+	mv picard-tools-1.123 picard
+  
+  
+# 4. get LOHHLO latest script as the old script bundle in the conda has some bugs.
+
+git clone https://bitbucket.org/mcgranahanlab/lohhla.git 
